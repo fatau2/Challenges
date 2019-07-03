02d2a55b12507aaee2dac18f006ac78d92ea8443
fatau2 <rekerism@gmail.com>
2019-07-03 08:56:44 +0700

    adding Tuesday challenges

---
+++ week2/jsfiles/conditional-if-else.js
@@ -0,0 +1,19 @@
+//console.log("helloworld");
+
+var nama = "farras";
+var peran = "penyihir";
+
+if(nama != ""){
+	console.log("welcome " + nama);
+	if(peran === "ksatria"){
+		console.log("you chose " + peran + " class")
+	} else if(peran === "tabib"){
+		console.log("you chose " + peran + " class")
+	} else if(peran === "penyihir"){
+		console.log("you chose " + peran + " class")
+	} else {
+		console.log("pilih kelas yang tersedia")
+	}
+
+}
+
---
+++ week2/jsfiles/conditional-switch.js
@@ -0,0 +1,60 @@
+var tanggal = "4";
+var bulan = "4";
+var tahun = "2019";
+
+
+switch(bulan){
+	case "1":
+		bulan = "January";
+		console.log(tanggal + " " + bulan + " " + tahun);
+		break;
+	case "2":
+                bulan = "February";
+                console.log(tanggal + " " + bulan + " " + tahun);
+                break;
+	case "3":
+                bulan = "March";
+                console.log(tanggal + " " + bulan + " " + tahun);
+                break;
+	case "4":
+                bulan = "April";
+                console.log(tanggal + " " + bulan + " " + tahun);
+                break;
+	case "5":
+                bulan = "May";
+                console.log(tanggal + " " + bulan + " " + tahun);
+                break;
+	case "6":
+                bulan = "June";
+                console.log(tanggal + " " + bulan + " " + tahun);
+                break;
+	case "7":
+                bulan = "July";
+                console.log(tanggal + " " + bulan + " " + tahun);
+                break;
+	case "8":
+                bulan = "August";
+                console.log(tanggal + " " + bulan + " " + tahun);
+                break;
+	case "9":
+                bulan = "September";
+                console.log(tanggal + " " + bulan + " " + tahun);
+                break;
+	case "10":
+                bulan = "October";
+                console.log(tanggal + " " + bulan + " " + tahun);
+                break;
+	case "11":
+                bulan = "November";
+                console.log(tanggal + " " + bulan + " " + tahun);
+                break;
+	case "12":
+                bulan = "December";
+                console.log(tanggal + " " + bulan + " " + tahun);
+		break;
+	default:
+                console.log("invalid date");
+                break;
+}
+
+
